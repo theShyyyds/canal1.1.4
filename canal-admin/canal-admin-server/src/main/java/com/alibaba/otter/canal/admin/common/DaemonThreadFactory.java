1e6271013f810b097907994ac25a88e611b5a5d5
wp_2 <wp_2@qq.com>
2022-07-29 09:40:46 +0800

    测试提交

--- canal-admin/canal-admin-server/src/main/java/com/alibaba/otter/canal/admin/common/DaemonThreadFactory.java
+++ canal-admin/canal-admin-server/src/main/java/com/alibaba/otter/canal/admin/common/DaemonThreadFactory.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ThreadFactory;
 public class DaemonThreadFactory implements ThreadFactory {
 
     public static final ThreadFactory daemonThreadFactory = new DaemonThreadFactory();
-
+    //test
     public Thread newThread(Runnable r) {
         Thread t = Executors.defaultThreadFactory().newThread(r);
         t.setDaemon(true);
